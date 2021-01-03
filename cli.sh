c15324910a91c5a2b4a9815f2f7b20605aa123f7
Thomas Burk <t.burk@bckx.de>
2021-01-03 16:46:22 +0100

    Check for requirements

--- cli.sh
+++ cli.sh
@@ -1,5 +1,29 @@
 #!/usr/bin/env bash
 
+prereqs_missing() {
+  echo
+  echo "Prerequisites missing!"
+  echo
+  echo "In order to use that script, you need to install"
+  echo "HTTPie (including the httpie-jwt-auth plugin) and jq."
+  echo
+  echo "On Debian based distributions, you can issue:"
+  echo
+  echo "sudo apt-get install httpie jq"
+  echo "pip3 -U install httpie-jwt-auth"
+  echo
+  echo "On MacOS, you can install via brew by issuing:"
+  echo
+  echo "brew install httpie jq"
+  echo "pip3 -U install httpie-jwt-auth"
+  echo
+  exit 1
+}
+
+command -v jq >/dev/null 2>&1 || prereqs_missing
+command -v http >/dev/null 2>&1 || prereqs_missing
+pip3 list 2>/dev/null | grep httpie-jwt-auth >/dev/null || prereqs_missing
+
 [ -r .env.cli ] && source .env.cli
 
 echo
